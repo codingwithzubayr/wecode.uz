0d00b89c9da4797e82fffa0cdc9e3c72515137da
doston701 <abduvohidovdoston0305@gmail.com>
2023-12-16 22:40:11 -0800

    Deleted next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,15 @@
-import Head from 'next/head';
-
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
   <>
-    <Head>
-      <title>wecode.uz</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://stijndv.com" />
-      <link
-        rel="stylesheet"
-        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
-      />
-    </Head>
+    <title>wecode.uz</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="preconnect" href="https://stijndv.com" />
+    <link
+      rel="stylesheet"
+      href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+    />
     <Component {...pageProps} />
   </>
 );
